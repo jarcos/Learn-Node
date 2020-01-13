7f80d00572368771826361f6d7c557ce3cab42fa
José Arcos <hello@josearcos.me>
2020-01-13 21:12:11 +0100

    Changes

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+const { catchErrors } = require('../handlers/errorHandlers');
+
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+
+router.post(
+  '/add',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore),
+);
+router.post(
+  '/add/:id',
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore),
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/stores/:slug', catchErrors(storeController.getStoreBySlug));
 
 module.exports = router;
